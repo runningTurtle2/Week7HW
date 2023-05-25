e7f86a1ab978ead3843f9c991657c21d7cd8df46
runningTurtle2 <oscarmoren2010@gmail.com>
2023-05-24 23:09:20 -0700

    keep data in server

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,60 +9,7 @@ let Game = function (pTitle, pGenre, pRelease, pURL, pArticle) {
   this.ArticalLink = pArticle
   this.ID = Math.random().toString(16).slice(5); // Unique ID per object created
 };
-gameArr.push(
-  new Game(
-    "Red Dead Redemption 2",
-    "Action; Open World",
-    "10/26/2018",
-    "https://www.youtube.com/embed/HVRzx17WHVk",
-    "https://www.ign.com/games/red-dead-redemption-2/articles"
-  )
-);
-gameArr.push(
-  new Game(
-    "Pokémon Legends: Arceus",
-    "Adventure; Open World",
-    "1/28/2022",
-    "https://www.youtube.com/embed/I4RynqpahT8",
-    "https://www.ign.com/games/pokemon-legends-arceus/articles"
-  )
-);
-gameArr.push(
-  new Game(
-    "Assassin's Creed Mirage",
-    "Action; Rogue-like",
-    "TBA 2023",
-    "https://www.youtube.com/embed/x55lAlFtXmw",
-    "https://www.ign.com/games/assassins-creed-mirage/articles"
-  )
-);
-gameArr.push(
-  new Game(
-    "Diablo 4",
-    "Action; Adventure",
-    "6/6/2023",
-    "https://www.youtube.com/embed/Ro26B394ZBM",
-    "https://www.ign.com/games/diablo-iv/articles"
-  )
-);
-gameArr.push(
-  new Game(
-    "The Legend of Zelda: Tears of the Kingdom",
-    "Action; Open World",
-    "5/12/2023",
-    "https://www.youtube.com/embed/uHGShqcAHlQ",
-    "https://www.ign.com/games/the-legend-of-zelda-tears-of-the-kingdom/articles"
-  )
-);
-gameArr.push(
-  new Game(
-    "Hollow Knight: Silksong",
-    "Action; Adventure",
-    "TBA 2023",
-    "https://www.youtube.com/embed/JSfuFlhsxZY",
-    "https://www.ign.com/games/hollow-knight-silksong/articles"
-  )
-);
+
 
 document.addEventListener("DOMContentLoaded", function () {
   // add button events
@@ -80,17 +27,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     let game = new Game(title, genre, release, url, article);
-    gameArr.push(game);
-    document.location.href = "index.html#library";
-    // clear the input values
-    document.getElementById("title").value = "";
-    document.getElementById("genre").value = "";
-    document.getElementById("release").value = "";
-    document.getElementById("URL").value = "";
-    document.getElementById("article"). value = "";
+    //gameArr.push(game);
 
-    // refresh library
-    //createList();
+    $.ajax({
+      url : "/addGame",
+      type: "POST",
+      data: JSON.stringify(game),
+      contentType: "application/json; charset=utf-8",
+      success: function(result){
+        console.log(result);
+
+        document.location.href = "index.html#library";
+        // clear the input values
+        document.getElementById("title").value = "";
+        document.getElementById("genre").value = "";
+        document.getElementById("release").value = "";
+        document.getElementById("URL").value = "";
+        document.getElementById("article"). value = "";
+      }
+    })
+
   });
 
   document.getElementById("buttonClear").addEventListener("click", function () {
@@ -110,19 +66,27 @@ function createList() {
   let myUL = document.getElementById("gameList");
   myUL.innerHTML = "";
 
-  gameArr.forEach(function (aGame) {
-    let myLi = document.createElement("li");
-    // adding a class name
-    myLi.classList.add("aGame");
-    myLi.setAttribute("data-parm", aGame.ID);
-    myLi.innerHTML = "<a href='" + aGame.ArticalLink  +
-      "' target='_blank'><span style='font-weight: bold;'>Title:</span> " + aGame.Title +
-      "<br><span style='font-weight: bold;'>Genre:</span> " + aGame.Genre +
-      "<br><span style='font-weight: bold;'>Release:</span> " + aGame.Release +
-      "</a><br>" +
-      '<iframe width="560" height="315" src="' + aGame.URL +
-      '" title= "YouTube video player"' +
-      'allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"allowfullscreen></iframe><br><br>';
-    myUL.appendChild(myLi);
+  //client code
+  $.get("/getGames", function (data, status) {
+    gameArr = data;
+
+    // all code tha will use this fresh data
+    gameArr.forEach(function (aGame) {
+      let myLi = document.createElement("li");
+      // adding a class name
+      myLi.classList.add("aGame");
+      myLi.setAttribute("data-parm", aGame.ID);
+      myLi.innerHTML = "<a href='" + aGame.ArticalLink +
+        "' target='_blank'><span style='font-weight: bold;'>Title:</span> " + aGame.Title +
+        "<br><span style='font-weight: bold;'>Genre:</span> " + aGame.Genre +
+        "<br><span style='font-weight: bold;'>Release:</span> " + aGame.Release +
+        "</a><br>" +
+        '<iframe width="560" height="315" src="' + aGame.URL +
+        '" title= "YouTube video player"' +
+        'allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"allowfullscreen></iframe><br><br>';
+      myUL.appendChild(myLi);
+    });
+
   });
-}
+  
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,110 @@
 var express = require('express');
 var router = express.Router();
 
+
+let gameArr = [];
+
+//Constructor for game object
+let Game = function (pTitle, pGenre, pRelease, pURL, pArticle) {
+  this.Title = pTitle;
+  this.Genre = pGenre;
+  this.Release = pRelease;
+  this.URL = pURL;
+  this.ArticalLink = pArticle
+  this.ID = Math.random().toString(16).slice(5); // Unique ID per object created
+};
+gameArr.push(
+  new Game(
+    "Red Dead Redemption 2",
+    "Action; Open World",
+    "10/26/2018",
+    "https://www.youtube.com/embed/HVRzx17WHVk",
+    "https://www.ign.com/games/red-dead-redemption-2/articles"
+  )
+);
+gameArr.push(
+  new Game(
+    "Pokémon Legends: Arceus",
+    "Adventure; Open World",
+    "1/28/2022",
+    "https://www.youtube.com/embed/I4RynqpahT8",
+    "https://www.ign.com/games/pokemon-legends-arceus/articles"
+  )
+);
+gameArr.push(
+  new Game(
+    "Assassin's Creed Mirage",
+    "Action; Rogue-like",
+    "TBA 2023",
+    "https://www.youtube.com/embed/x55lAlFtXmw",
+    "https://www.ign.com/games/assassins-creed-mirage/articles"
+  )
+);
+gameArr.push(
+  new Game(
+    "Diablo 4",
+    "Action; Adventure",
+    "6/6/2023",
+    "https://www.youtube.com/embed/Ro26B394ZBM",
+    "https://www.ign.com/games/diablo-iv/articles"
+  )
+);
+gameArr.push(
+  new Game(
+    "The Legend of Zelda: Tears of the Kingdom",
+    "Action; Open World",
+    "5/12/2023",
+    "https://www.youtube.com/embed/uHGShqcAHlQ",
+    "https://www.ign.com/games/the-legend-of-zelda-tears-of-the-kingdom/articles"
+  )
+);
+gameArr.push(
+  new Game(
+    "Hollow Knight: Silksong",
+    "Action; Adventure",
+    "TBA 2023",
+    "https://www.youtube.com/embed/JSfuFlhsxZY",
+    "https://www.ign.com/games/hollow-knight-silksong/articles"
+  )
+);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html');
+});
+
+/**Get all game data */
+router.get('/getGames', function(req, res){
+  res.status(200).json(gameArr);
+})
+
+/**Add a new game */
+router.post('/addGame', function(req, res){
+  const newGame = req.body;
+  gameArr.push(newGame);
+  res.status(200).json(newGame);
+});
+
+// add route for delete
+router.delete('/deleteGame/:ID', (req, res) => {
+  console.log('in server');
+  const delID = req.params.ID;
+
+  let found = false;
+  let pointer = GetArrayPointer(delID);
+  //if did not find game in array
+  if(pointer == -1){
+    console.log("not found");
+    return res.status(500).json({
+      status: "error - no such ID"
+    });
+  }
+  // if did find the game
+  else{
+    gameArr.splice(pointer, 1); // remove 1 element at index
+    res.send('Game with ID: ' + delID + ' deleted!');
+  }
 });
 
 module.exports = router;
